d541db6501f132ff83d99f38ae85ea4d58613045
St. 1 <spirit9969@gmail.com>
2020-12-15 23:47:17 +0000

    Add exaple destruct

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+//Destructurisation
+var sandwich = {
+bread: "dutch crunch",
+meat: "tuna",
+cheese: "swiss",
+toppings: ["lettuce", "tomato", "mustard"]
+}
+var {bread, meat} = sandwich
+console.log(bread, meat)
+
+var regularPerson={
+  firstName:"Nick",
+  lastname:"Willson"
+}
+var lordify=({firstName})=>{
+  console.log(`${firstName} of Canterbuy`)
+}
+lordify(regularPerson)
+
+
+
+//(...)join arrays
+var peaks=["Tara", "Stas","Sara"]
+var canyas=["Mersede", "Josh"]
+var tahoe=[...peaks, ...canyas]
+console.log(tahoe.join(', '))
+
+var [last] = peaks.reverse()
+console.log(last)
+
+var[first,...rest]= peaks
+console.log(rest.join(", "))
+
 function directions(...args) {
 var [start, ...remaining] = args
 var [finish, ...stops] = remaining.reverse()
@@ -13,3 +46,14 @@ directions(
 "Homewood",
 "Tahoma"
 )
+
+var morning= {
+  breakfast:"oatmel",
+  lunch: "peanut butter"
+}
+var dinner = "mac and chees"
+var backpackimgMeals ={
+  ...morning,
+  dinner
+  }
+  console.log(backpackimgMeals)
